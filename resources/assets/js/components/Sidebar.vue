5960fb6faa5bb17d7116cf1d07ef0cd49f4a7cfc
Aldrin Navarro <aldrinnavarro16@gmail.com>
2018-04-16 20:56:21 +0800

    Add sidebar vue component

---
+++ resources/assets/js/components/Sidebar.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="col-12 col-md-3 col-xl-2 bd-sidebar">
+        <div
+            v-for="element in elements"
+            v-bind:key="element.id">
+            <h6>{{ element.text }}</h6>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function() {
+            return {
+                elements: [
+                    {
+                        id: 1,
+                        icon: 'fire',
+                        incidentType: 'fire',
+                        location: [123.00, 56.00],
+                        text: 'Olango Island'
+                    }, {
+                        id: 2,
+                        icon: 'fire',
+                        incidentType: 'fire',
+                        location: [123.00, 56.00],
+                        text: 'Maribago Mactan'    
+                    }
+                ]
+            }
+        }
+    }
+</script>
