7d0437304f7e12259f96f7e2aca68def9b35c644
Aldrin Navarro <aldrinnavarro16@gmail.com>
2018-05-06 03:38:15 +0800

    Add is_resolved is_validated fields

--- app/Report.php
+++ app/Report.php
@@ -13,5 +13,5 @@ class Report extends Model
 	*
 	* @var array
 	*/
-	protected $fillable = ['owner_id', 'incident_id', 'lat', 'lng','body','status','address','contact_number'];
+	protected $fillable = ['owner_id', 'incident_id', 'lat', 'lng','body','status','address','contact_number', 'is_validate', 'is_resolved'];
 }
---
+++ database/migrations/2018_05_05_191056_report_statuses.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ReportStatuses extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('reports', function (Blueprint $table) {
+            $table->boolean('is_validated')->default(0);
+            $table->boolean('is_resolved')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
