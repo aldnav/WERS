43ef0ff2e14a8526d20695102578b801b6cced11
Aldrin Navarro <aldrinnavarro16@gmail.com>
2018-05-06 03:40:06 +0800

    Get reports. Set fillable user_role

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'contact_number'
+        'name', 'email', 'password', 'contact_number', 'user_role'
     ];
 
     /**
@@ -26,4 +26,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function reports()
+    {
+        return $this->hasMany('App\Report', 'owner_id');
+    }
 }
