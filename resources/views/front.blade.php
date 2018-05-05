b6db3d955263b63b6bfda8cb31ea11216205498e
Aldrin Navarro <aldrinnavarro16@gmail.com>
2018-05-05 22:26:00 +0800

    Reorganize add report form

--- resources/views/front.blade.php
+++ resources/views/front.blade.php
@@ -12,3 +12,26 @@
         </div>
     </div>
 @endsection
+
+
+@section('add_report')
+<modal v-if="showModal" @close="showModal = false">
+    <template slot='header'>
+    <div class="panel-default clearfix">
+        <h5 class="panel-title float-left">Add a report</h5>
+        <button type="button" class="close float-right" aria-label="Close" @click="showModal = false"><span aria-hidden="true">&times;</span></button>
+    </div>
+    </template>
+    <template slot='body'>
+        @if(Auth::guest())  
+        <div class="panel-body">                
+        <report-form :user-id= "''" :user-contact="''" @close="showModal = false"></report-form>
+        </div>
+        @else
+        <div class="panel-body">
+        <report-form :user-id= "{{ Auth::id() }}" @close="showModal = false"></report-form>
+        </div>
+        @endif
+    </template>
+</modal>
+@endsection
