f875d53083fe5de346592095c434e9edd752998c
Aldrin Navarro <aldrinnavarro16@gmail.com>
2018-05-05 22:26:33 +0800

    Redesign add report form

--- resources/assets/js/components/ModalComponent.vue
+++ resources/assets/js/components/ModalComponent.vue
@@ -1,20 +1,20 @@
 <template id="modal-template">
   <transition name="modal">
-    <div class="modal-mask pull-right">
+    <div class="modal-mask">
       <div class="modal-wrapper ">
         <div class="modal-container ">
-          <div class="panel panel-primary">
+          <div class="panel panel-primary panel-header">
             <slot name="header">
               default header
             </slot>
+          </div>
           <div class="panel-body">
             <slot name='body'>
               default body
             </slot>
-          </div> 
-          </div>
           </div>
         </div>
       </div>
+    </div>
   </transition>
 </template>
