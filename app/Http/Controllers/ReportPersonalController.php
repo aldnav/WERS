82530239ca588533839630a9d6ecabf8979a72cc
Aldrin Navarro <aldrinnavarro16@gmail.com>
2018-05-06 08:55:22 +0800

    Accept resolution note

--- app/Http/Controllers/ReportPersonalController.php
+++ app/Http/Controllers/ReportPersonalController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\User;
 use App\Report;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Contracts\Auth\Authenticatable;
 
@@ -29,11 +30,23 @@ class ReportPersonalController extends Controller
         ]);
     }
 
-    public function validate($id, $userid, $resolve = 0) {
+    public function validate(Request $request, $id, $userid, $resolve = 0) {
         $report = Report::find($id)->where('owner_id', '=', $userid)->first();
         $report->is_validated = true;
         if ($resolve == 1) {
             $report->is_resolved = true;
+        }
+        if ($request->resolution_note) {
+            $report->resolution_note = 'Validated.' . $request->resolution_note;
+        }
+        $report->save();
+    }
+
+    public function reject(Request $request, $id, $userid) {
+        $report = Report::find($id)->where('owner_id', '=', $userid)->first();
+        $report->is_rejected = true;
+        if ($request->resolution_note) {
+            $report->resolution_note = 'Rejected.' . $request->resolution_note;
         }
         $report->save();
     }
