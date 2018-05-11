d7672c81b01038788ec1b4785c3b11321f99a3ec
Aldrin Navarro <aldrinnavarro16@gmail.com>
2018-05-11 20:33:10 +0800

    Force https on boot

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,6 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        \URL::forceScheme('https'); // force https
         /**
          * Notification
          * 1. Who to send notif
