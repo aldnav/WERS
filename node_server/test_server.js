75f482a35cd47be9ba20b748ab814624a433d14e
Aldrin Navarro <aldrinnavarro16@gmail.com>
2018-05-12 14:53:46 +0800

    Change port

--- node_server/test_server.js
+++ node_server/test_server.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(8080, process.env.IP_ADDRESS);
-console.log(`Server running at http://${process.env.IP_ADDRESS}:8080/`);
+}).listen(9090, process.env.IP_ADDRESS);
+console.log(`Server running at http://${process.env.IP_ADDRESS}:9090/`);
